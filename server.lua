2dd8d728d57b1361d98aa6d2a7e006d8f6030875
tie372 <tie.372@gmail.com>
2013-08-22 00:54:02 -0600

    Basic skeleton;

---
+++ server.lua
@@ -0,0 +1,12 @@
+local http = require 'http'
+
+http.createServer(function(req, res)
+  local body = 'It works!'
+  res:writeHead(200, {
+    Content-Type = 'text/plain',
+    Content-Length = #body
+  })
+  res:finish(body)
+end):listen(8080)
+
+print('Listening on 8080')
