77a0dbdac1370873482fc2d1d9d7f430ef0f0ef2
tie372 <tie.372@gmail.com>
2013-08-27 20:28:34 -0600

    Add routing; Add static file handling;
    Ditched journey for director.
    Basic homepage is up.

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
-var http = require('http');
+var fs = require('fs'),
+    http = require('http'),
+    ejs = require('ejs'),
+    static = require('node-static'),
+    director = require('director');
 
-var User = require('./models/user');
-var Room = require('./models/room');
+var User = require('./models/user'),
+    Room = require('./models/room');
+
+function index() {
+	var res = this.res;
+	res.writeHead(200, {'Content-type': 'text/html'});
+	fs.readFile('./views/index.ejs', 'utf8', function(e, view) {
+		res.end(ejs.render(view));
+	});	
+}
+
+var router = new director.http.Router({
+	'/': {
+		get: function() {
+			var res = this.res;
+			res.writeHead(200, {'Content-type': 'text/html'});
+			fs.readFile('./views/index.ejs', 'utf8', function(e, view) {
+				res.end(ejs.render(view));
+			});	
+		}
+	}
+});
+
+var files = new static.Server('./static');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Baccano\n');
+	router.dispatch(req, res, function(e) {
+		if(e) {
+			return files.serve(req, res, function() {
+				res.writeHead(404);
+				res.end('404');
+			});
+		}
+	});
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
